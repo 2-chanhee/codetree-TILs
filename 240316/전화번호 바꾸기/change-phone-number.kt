7d0ec7aa51d300d091765cd3357ff10f778ff98a
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2024-03-16 19:39:51 +0900

    Title: 전화번호 바꾸기, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

--- 240316/전화번호 바꾸기/change-phone-number.kt
+++ 240316/전화번호 바꾸기/change-phone-number.kt
@@ -1,8 +1,8 @@
-fun main (){
+fun main () {
     val phoneNumber = readLine();
     // let: null이 아닐 때만 호출
     phoneNumber?.let {
-        val split = phoneNumber!!.split('-');
+        val split: List<String> = phoneNumber.split('-');
         print("${split[0]}-${split[2]}-${split[1]}")
     }  
 }
