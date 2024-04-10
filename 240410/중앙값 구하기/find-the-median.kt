c164f28d7c411a26ea6d66d0def7fdc26e17d00b
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2024-04-10 21:36:18 +0900

    Title: 중앙값 구하기, Time: 170ms, Memory: 16MB, Status: Passed - CodeTree

--- 240410/중앙값 구하기/find-the-median.kt
+++ 240410/중앙값 구하기/find-the-median.kt
@@ -7,16 +7,6 @@ fun main () {
     val intB = sb.toIntOrNull()
     val intC = sc.toIntOrNull()
 
-    var center = 0;
-
-    if(intA != null && intB != null && intC != null) {
-        if((intA < intB) && (intA < intC)) {
-            if(intB < intC) center = intB else center = intC
-        }else if ((intB < intA) && (intB < intC)) {
-            if(intA < intC) center = intA else center = intC
-        }else if ((intC < intA) && (intC < intB)) {
-            if(intA < intB) center = intA else center = intB
-        }
-    }
-    print(center)
+    val numbers = listOf(intA, intB, intC).filterNotNull().sorted();
+    print(numbers[1])
 }
