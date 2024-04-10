191e65d8af716a8e3d28de5bf38d04a48110fb47
2-chanhee <54311184+2-chanhee@users.noreply.github.com>
2024-04-10 21:22:02 +0900

    Title: 두 사람, Time: 148ms, Memory: 13MB, Status: Passed - CodeTree

--- 240410/두 사람/two-person.kt
+++ 240410/두 사람/two-person.kt
@@ -6,8 +6,11 @@ fun main() {
         val (info1Age, info1Sex) = info1.split(" ")
         val (info2Age, info2Sex) = info2.split(" ")
 
-        if(info1Age != null && info2Age !== null ) {
-            return if((info1_age >= 19 && info1_sex == "M") || (info2_age >= 19 && info2_sex == "M")) print(1) else print(0)
+        val info1AgeInt = info1Age.toIntOrNull()
+        val info2AgeInt = info2Age.toIntOrNull()
+
+        if(info1AgeInt != null && info2AgeInt !== null ) {
+            return if((info1AgeInt >= 19 && info1Sex == "M") || (info2AgeInt >= 19 && info2Sex == "M")) print(1) else print(0)
         }
     }
 }
